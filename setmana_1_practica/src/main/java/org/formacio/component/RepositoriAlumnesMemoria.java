0992b2efb6effcc79d60521a974a273707a97fba
samuFerragut <sferragut@cifpfbmoll.eu>
2020-04-30 11:27:07 +0200

    feat: Modificar clase a componente

--- setmana_1_practica/src/main/java/org/formacio/component/RepositoriAlumnesMemoria.java
+++ setmana_1_practica/src/main/java/org/formacio/component/RepositoriAlumnesMemoria.java
@@ -4,10 +4,13 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 /**
  * A part del que sigui necessari per fer d'aquesta classe un component, no s'ha de 
  * modificar res mes d'aquesta classe
  */
+@Component
 public class RepositoriAlumnesMemoria implements RepositoriAlumnes {
 
 	private Map<Integer, String> bbdd = new HashMap<>();

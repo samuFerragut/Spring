4f32df15a0d1627bc7a25f44e18ca65795f59d10
samuFerragut <sferragut@cifpfbmoll.eu>
2020-06-29 11:52:21 +0200

    semana 1

--- README.md
+++
@@ -1,3 +0,0 @@
-# Curso Spring
-
-Este curso consta de una serie de ejercicios usando spring. Subiré un total de 4 ejercicios 1 por semana.
---
+++ setmana_1_components_exercici/src/main/java/org/formacio/component/ClientCotitzacionsWS.java
@@ -0,0 +1,26 @@
+package org.formacio.component;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//aquesta classe ha de ser detectada com un component
+@SpringBootApplication
+
+public class ClientCotitzacionsWS implements IntegradorCotitzacions {
+
+	public int nombreInvocacions = 0;
+	
+	public float obteMitjanaDiariaCotitzacions() {
+		nombreInvocacions++; // per controls del test
+		return 20f;
+	}
+	
+	@Override
+	public float obteCotitzacio(String empresa) {
+		switch (empresa) {
+		     case "cervesses.sa": return 30f;
+		     case "shandies.sa": return 10f;
+		     default : return 20f;
+		}
+	}
+
+}
